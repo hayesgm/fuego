a340ef3c97f1bb93a51d4ec588e25318a40bd1d2
Geoff Hayes <hayesgm@gmail.com>
2015-05-11 23:55:42 -0700

    Add prod config back with keys

---
+++ config/prod.secret.exs
@@ -0,0 +1,16 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :fuego, Fuego.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :fuego, Fuego.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: {:system, "DATABASE_URL"}
+
+config :cqerl,
+  cassandra_nodes: Enum.map(String.split(System.get_env("CASSANDRA_NODES"), ","), fn ip -> [ip, 9042] end),
+  keyspace: "fuego_prod"
