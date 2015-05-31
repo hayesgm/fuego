76ec4614c715a720d113dfc8a3baa5536cfe98e1
Geoff Hayes <hayesgm@gmail.com>
2015-05-31 10:32:49 -0700

    add excessive debug info

--- brunch-config.js
+++ brunch-config.js
@@ -1,4 +1,27 @@
-console.log("loaded config...");
+
+var _getAllFilesFromFolder = function(dir) {
+
+    var filesystem = require("fs");
+    var results = [];
+
+    filesystem.readdirSync(dir).forEach(function(file) {
+
+        file = dir+'/'+file;
+        var stat = filesystem.statSync(file);
+
+        if (stat && stat.isDirectory()) {
+            results = results.concat(_getAllFilesFromFolder(file))
+        } else results.push(file);
+
+    });
+
+    return results;
+
+};
+
+_getAllFilesFromFolder(process.cwd()).forEach(function(file) {
+  console.log("File: " + file);
+});
 
 exports.config = {
   // See http://brunch.io/#documentation for docs.
