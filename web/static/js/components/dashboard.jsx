98c3e50e08b6d828d02f8c9565bfb08e2fed3d50
Geoff Hayes <hayesgm@gmail.com>
2015-05-30 17:33:42 -0700

    Start to add some copy to dashboard page

--- web/static/js/components/dashboard.jsx
+++ web/static/js/components/dashboard.jsx
@@ -4,7 +4,11 @@ export class Dashboard extends React.Component {
   render() {
     return (
       <div className="dashboard">
-        The Dashboard
+        <h1>Dashboard</h1>
+
+        <p>
+          Burn.pm makes it easy to send files directly to friends or co-workers.
+        </p>
       </div>
     )
   }
